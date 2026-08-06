483c95c060a4d223273260d148592f56b7eae6e5
Lain <chiyomiziwari@gmail.com>
2026-08-06 11:31:43 +0700

    Add GET endpoint for branch stock list by ID
    Added GET api/branches/{id}/stock-list to BranchesController to retrieve all product stock for a branch using GetAllProductStockByBranchIdQuery and ToActionResult.

--- src/Restaurant.API/Controllers/Territory/BranchesController.cs
+++ src/Restaurant.API/Controllers/Territory/BranchesController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Restaurant.API.Extensions;
+using Restaurant.Application.Features.Inventory.ProductStocks.Queries.GetAllByBranchId;
 using Restaurant.Application.Features.Territory.Branches.Queries.GetAll;
 
 namespace Restaurant.API.Controllers.Territory
@@ -16,6 +17,16 @@ namespace Restaurant.API.Controllers.Territory
             [FromQuery] GetAllBranchesQuery query,
             CancellationToken cancellationToken)
         {
+            var result = await _mediator.Send(query, cancellationToken);
+            return this.ToActionResult(result);
+        }
+
+        [HttpGet("{id}/stock-list")]
+        public async Task<IActionResult> GetAllStock(
+            [FromRoute] string id,
+            CancellationToken cancellationToken)
+        {
+            var query = new GetAllProductStockByBranchIdQuery(id);
             var result = await _mediator.Send(query, cancellationToken);
             return this.ToActionResult(result);
         }
