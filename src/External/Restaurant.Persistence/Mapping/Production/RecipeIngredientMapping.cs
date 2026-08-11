984f7df4a3bafe5fb0987549698e15fd4db22bf2
Lain <chiyomiziwari@gmail.com>
2026-08-11 16:07:57 +0700

    Add AutoMapper profiles and response DTOs for recipes
    Introduce Recipe and RecipeIngredient mapping profiles using AutoMapper. Map entity fields to new RecipeResponse and RecipeIngredientResponse DTOs for API responses, including related names for Product, Ingredient, and Unit. Add DTO classes to represent response structures.

---
+++ src/External/Restaurant.Persistence/Mapping/Production/RecipeIngredientMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Restaurant.Contract.DTOs.Production.RecipeIngredients;
+using Restaurant.Domain.Entities.Production;
+
+namespace Restaurant.Persistence.Mapping.Production
+{
+    internal class RecipeIngredientMapping : Profile
+    {
+        public RecipeIngredientMapping()
+        {
+            CreateMap<RecipeIngredient, RecipeIngredientResponse>()
+                .ForMember(dest => dest.IngredientName, opt => opt.MapFrom(src => src.Ingredient.Name))
+                .ForMember(dest => dest.UnitName, opt => opt.MapFrom(src => src.Unit.Name));
+        }
+    }
+}
---
+++ src/External/Restaurant.Persistence/Mapping/Production/RecipeMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Restaurant.Contract.DTOs.Production.Recipes;
+using Restaurant.Domain.Entities.Production;
+
+namespace Restaurant.Persistence.Mapping.Production
+{
+    internal class RecipeMapping : Profile
+    {
+        public RecipeMapping()
+        {
+            CreateMap<Recipe, RecipeResponse>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.PublicId))
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.RecipeIngredients, opt => opt.MapFrom(src => src.RecipeIngredients));
+        }
+    }
+}
